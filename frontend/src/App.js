6ed81341fd1794b516e94b760478fa775643c4ad
ronaldo.caetano <ronaldogodoi2@gmail.com>
2020-02-09 16:01:01 -0300

    criando css da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,93 @@
 import React from 'react';
+import './Global.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
 	return (
-		<div className="App">
-			<span>Teste</span>
+		<div id="app">
+			<aside>
+				<strong>Cadastrar</strong>
+				<form>
+					<div className="input-block">
+						<label htmlFor="github_username">Usuário do github</label>
+						<input type="text" name="github_username" id="github_username"/>
+					</div>
+					<div className="input-block">
+						<label htmlFor="techs">Tecnologias</label>
+						<input type="text" name="techs" id="techs" />
+					</div>
+					<div class="input-group">
+						<div className="input-block">
+							<label htmlFor="latitude">Latitude</label>
+							<input type="text" name="latitude" id="latitude" />
+						</div>
+						<div className="input-block">
+							<label htmlFor="longitude">Longitude</label>
+							<input type="text" name="longitude" id="longitude" />
+						</div>
+					</div>
+				</form>
+			</aside>
+			<main>
+				<ul>
+					<li className="dev-item">
+						<header>
+							<img />
+							<div className="user-info">
+								<strong></strong>
+								<span></span>
+							</div>
+						</header>
+						<p></p>
+						<a href="https://gtihub.com/">Acessar perfil no GitHub</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img />
+							<div className="user-info">
+								<strong></strong>
+								<span></span>
+							</div>
+						</header>
+						<p></p>
+						<a href="https://gtihub.com/">Acessar perfil no GitHub</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img />
+							<div className="user-info">
+								<strong></strong>
+								<span></span>
+							</div>
+						</header>
+						<p></p>
+						<a href="https://gtihub.com/">Acessar perfil no GitHub</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img />
+							<div className="user-info">
+								<strong></strong>
+								<span></span>
+							</div>
+						</header>
+						<p></p>
+						<a href="https://gtihub.com/">Acessar perfil no GitHub</a>
+					</li>
+					<li className="dev-item">
+						<header>
+							<img />
+							<div className="user-info">
+								<strong></strong>
+								<span></span>
+							</div>
+						</header>
+						<p></p>
+						<a href="https://gtihub.com/">Acessar perfil no GitHub</a>
+					</li>
+				</ul>
+			</main>
 		</div>
 	);
 }
